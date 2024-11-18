199082d8381630028a2713e06531ed56dae6fa39
BLSSMA <162845981+BLSSMA@users.noreply.github.com>
2024-11-18 17:53:58 -0500

    made homepage responsive

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -15,29 +15,29 @@ return(
             <div className="grid">
                 <div className="row">
                     <div className="strikethrough">____________________________</div>
-                    <div className="col-12 title">LOCAL CBD COMPANY</div>
+                    <div className="col-md-12 title">LOCAL CBD COMPANY</div>
                 </div>
                 <div className="row">
-                    <div className="col-6 bottlesandcaps">
+                    <div className="col-md-6 bottlesandcaps">
                         <img src={bottlesandcaps} alt="bottles and caps"/>
                     </div>
-                    <div className="col-6 tagline">
+                    <div className="col-md-6 tagline">
                         <img src={tagline} alt="tagline" />
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-6 shoptag">
+                    <div className="col-md-6 shoptag">
                         <img src={shoptag} alt="shoptag" />
                     </div>
-                    <div className="col-6 bears">
+                    <div className="col-md-6 bears">
                         <img src={bears} alt="gummy bears"/>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-8 Vector">
+                    <div className="col-md-8 Vector">
                         <img src={Vector} alt="curly arrow"/>
                     </div>
-                    <div className="col-4">
+                    <div className="col-md-4">
                         <a href="/">SHOP</a>
                     </div>
                 </div>
